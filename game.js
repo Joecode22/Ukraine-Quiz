941b5317649103e27b643e17fc67a3fabf5c181c
josik <joe.code.mckinney@gmail.com>
2023-04-11 20:38:39 -0500

    corrected functionality of answer comparison, score, and styling

--- game.js
+++ game.js
@@ -1,8 +1,9 @@
-const question = document.getElementById('question');
-const choices = Array.from(document.getElementsByClassName('choice-text'));
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
 
 let currentQuestion = {}; //object to hold the current question
-let acceptingAnswers = false; //boolean to check if we are accepting answers
+let acceptingAnswers = true; //boolean to check if we are accepting answers
 let score = 0; //initialize the score to zero
 let questionCounter = 0; //initialize the question counter to zero
 let availableQuestions = []; //initialize the available questions array
@@ -15,43 +16,47 @@ let questions = [
     choice2: "September 4, 2022",
     choice3: "December 18, 2022",
     choice4: "February 24 2022",
-    answer: 4
+    answer: 4,
   },
 
   {
-    question: "Which southern Ukrainian city was assaulted, on 26 February 2022 by 12 tanks and 120 Russian soldiers but was successfully defended?",
+    question:
+      "Which southern Ukrainian city was assaulted, on 26 February 2022 by 12 tanks and 120 Russian soldiers but was successfully defended?",
     choice1: "Kharkiv",
-    choice2: 'Mykolaiv',
+    choice2: "Mykolaiv",
     choice3: "Kiev",
     choice4: "Odessa",
-    answer: 2
+    answer: 2,
   },
 
   {
-    question: "What language was primarily spoken by the Ukrainian Defenders during the battle of Mykolaiv?",
+    question:
+      "What language was primarily spoken by the Ukrainian Defenders during the battle of Mykolaiv?",
     choice1: "Russian",
     choice2: "Ukrainian",
     choice3: "English",
     choice4: "German",
-    answer: 1
+    answer: 1,
   },
 
   {
-    question: "Which southern Ukrainian city was captured by Russian forces on March 2, 2022?",
+    question:
+      "Which southern Ukrainian city was captured by Russian forces on March 2, 2022?",
     choice1: "Kherson",
     choice2: "Mykolaiv",
     choice3: "Kiev",
     choice4: "Odessa",
-    answer: 2
+    answer: 2,
   },
 
   {
-    question: "When did the Russians abandon and retreat from the city of Kherson?",
+    question:
+      "When did the Russians abandon and retreat from the city of Kherson?",
     choice1: "March 2, 2022",
     choice2: "May 10, 2022",
     choice3: "November 9, 2022",
     choice4: "March 17, 2023",
-    answer: 3
+    answer: 3,
   },
 
   {
@@ -60,22 +65,23 @@ let questions = [
     choice2: "Moscow",
     choice3: "New York",
     choice4: "London",
-    answer: 1
+    answer: 1,
   },
   {
-    question: "What is the name of the nuclear power plant that was captured by Russian forces on March 4, 2022?",
+    question:
+      "What is the name of the nuclear power plant that was captured by Russian forces on March 4, 2022?",
     choice1: "Chernobyl",
     choice2: "South Ukraine Nuclear Power Plant",
     choice3: "Nedobudovana Odes'ka Nuclear PowerPlant",
     choice4: "Zaporizhzhya Nuclear Power Plant",
-    answer: 4
+    answer: 4,
   },
-]
+];
 
 //constants
 const CORRECT_BONUS = 10; //points for correct answer
-const MAX_QUESTIONS = 3; //number of questions
-
+const MAX_QUESTIONS = 5; //number of questions
+//function to start the game
 function startGame() {
   questionCounter = 0; //reset question counter
   score = 0; //reset score
@@ -84,29 +90,45 @@ function startGame() {
 }
 
 //function to get a new question
-function getNewQuestion(){
-  questionCounter++; //increment the question counter
-  const questionIndex = Math.floor(Math.random() * availableQuestions.length); //generate a random question index
-  currentQuestion = availableQuestions[questionIndex]; //set the current question to the random question
-  question.innerText = currentQuestion.question; //set the question text to the current question
+function getNewQuestion() {
+  if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    return window.location.assign("/end.html");
+  }
+
+  questionCounter++; 
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length); 
+  currentQuestion = availableQuestions[questionIndex]; 
+  question.innerText = currentQuestion.question; 
 
   //loop through the choices
-  choices.forEach(choice => {
-    const number = choice.dataset['number']; //get the number of the choice
-    choice.innerText = currentQuestion['choice' + number]; //set the choice text to the current question's choice
+  choices.forEach((choice) => {
+    const number = choice.dataset["number"]; 
+    choice.innerText = currentQuestion["choice" + number]; 
   });
+  availableQuestions.splice(questionIndex, 1); 
+}
 
-  availableQuestions.splice(questionIndex, 1); //remove the question from the available questions array
-  acceptingAnswers = true; //set accepting answers to true
-};
+choices.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    if (!acceptingAnswers) return;
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
-choices.forEach(choice => {
-  choice.addEventListener('click', e => {
-    console.log(e.target); //log the target of the click event
+    if (classToApply === "correct") {
+      score += CORRECT_BONUS;
+    }
+
+    selectedChoice.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.classList.remove(classToApply);
+      acceptingAnswers = true;
+      getNewQuestion();
+    }, 1000);
   });
 });
 
-
 startGame();
-
-
